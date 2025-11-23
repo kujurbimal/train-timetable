75914aa673c2c7c07060be7c30a81799a2204774
BIMAL P.R. KUJUR <33995620+kujurbimal@users.noreply.github.com>
2025-11-23 18:42:57 +0530

    Create uk.py

---
+++ backend/providers/uk.py
@@ -0,0 +1,5 @@
+import os
+
+
+def get_trains(req):
+return {"provider":"National Rail (demo)","provider_status":"demo","trains":[{"train":"GWR 1256","depart":"12:10","arrive":"14:45","duration":"2h 35m"}]}
